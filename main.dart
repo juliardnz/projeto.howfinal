6195ca3a170766a8d9e55c47918d3c3345a5b32c
juliardnz <89615651+juliardnz@users.noreply.github.com>
2023-07-08 21:32:53 -0300

    projeto.how
    updade

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:projetohow/pages/client_list_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: ClientListPage()
+    );
+
+  }
+}
+
